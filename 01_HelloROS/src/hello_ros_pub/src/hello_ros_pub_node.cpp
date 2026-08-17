57fe7c3cb67ec55e77f441b87294e5765d020510
wanghan1232 <xkrj_0930@qq.com>
2026-08-18 00:01:58 +0800

    Implement custom ROS message publisher and subscriber

--- 01_HelloROS/src/hello_ros_pub/src/hello_ros_pub_node.cpp
+++ 01_HelloROS/src/hello_ros_pub/src/hello_ros_pub_node.cpp
@@ -4,29 +4,50 @@
 #include <chrono>
 #include <memory>
 
+#include "robot_pose_msgs/msg/robot_pose_msg.hpp"
 using namespace std::chrono_literals;
 
 int main(int argc, char **argv)
 {
+
+  float x = 0.0f;
+  float y = 0.0f;
+  float z = 0.0f;
+
+  float angle0 = 0.0f;
+  float angle1 = 0.0f;
+  float angle2 = 0.0f;
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<rclcpp::Node>("hello_ros_pub_node");
 
   auto publisher =
-    node->create_publisher<std_msgs::msg::String>(
-      "hello_ros_pub_topic", 10);
+      node->create_publisher<robot_pose_msgs::msg::RobotPoseMsg>(
+          "robot_pose", 10);
 
   auto timer = node->create_wall_timer(
-    1000ms,
-    [node, publisher]()
-    {
-      std_msgs::msg::String message;
-      message.data = "ros";
+      1000ms,
+      [publisher, &x, &y, &z, &angle0, &angle1, &angle2]() 
+      {
+        robot_pose_msgs::msg::RobotPoseMsg message;
 
-        message.data.c_str();
+        message.x = x;
+        message.y = y;
+        message.z = z;
 
-      publisher->publish(message);
-    });
+        message.angles.data = {
+            angle0,
+            angle1,
+            angle2};
+        publisher->publish(message);
+        x += 0.1f;
+        y += 0.1f;
+        z += 0.1f;
+
+        angle0 += 0.1f;
+        angle1 += 0.1f;
+        angle2 += 0.1f;
+      });
 
   rclcpp::spin(node);
   rclcpp::shutdown();
--- 01_HelloROS/src/hello_ros_sub/src/hello_ros_sub_node.cpp
+++ 01_HelloROS/src/hello_ros_sub/src/hello_ros_sub_node.cpp
@@ -1,20 +1,32 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-#include<memory>
+#include <memory>
+
+#include "robot_pose_msgs/msg/robot_pose_msg.hpp"
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<rclcpp::Node>("hello_ros_sub_node");
-
   auto subscription =
-    node->create_subscription<std_msgs::msg::String>(
-      "hello_ros_pub_topic", 10,
-      [node](std_msgs::msg::String::SharedPtr msg) {
-        RCLCPP_INFO_STREAM(node->get_logger(), msg->data);
-      });
+
+      node->create_subscription<robot_pose_msgs::msg::RobotPoseMsg>(
+          "robot_pose", 10, [node](robot_pose_msgs::msg::RobotPoseMsg::SharedPtr msg){
+        RCLCPP_INFO(
+          node->get_logger(),
+          "x=%.2f, y=%.2f, z=%.2f",
+          msg->x,
+          msg->y,
+          msg->z);
+    for (size_t i = 0; i < msg->angles.data.size(); i++) {
+        RCLCPP_INFO(
+          node->get_logger(),
+          "angles[%zu] = %.2f",
+          i,
+          msg->angles.data[i]);
+      } });
 
   rclcpp::spin(node);
   rclcpp::shutdown();
